30e91c1ccb01018cd6d87502cf0c073c52597484
camcol <cameroncoles816@gmail.com>
2024-08-11 21:03:04 -0230

    uploading completed testing files

---
+++ project/src/test/java/com/keyin/project/controller/AnimeControllerTest.java
@@ -0,0 +1,20 @@
+package com.keyin.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimeControllerTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getAllAnime() {
+    }
+
+    @Test
+    void search() {
+    }
+}
---
+++ project/src/test/java/com/keyin/project/controller/StudentControllerTest.java
@@ -0,0 +1,16 @@
+package com.keyin.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void getAllStudents() {
+    }
+}
