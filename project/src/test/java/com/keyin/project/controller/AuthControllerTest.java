55f4d68b86c065d5fb7d67a78ed9a43b0fa61410
camcol <cameroncoles816@gmail.com>
2024-08-11 22:57:08 -0230

    Create AuthControllerTest.java

---
+++ project/src/test/java/com/keyin/project/controller/AuthControllerTest.java
@@ -0,0 +1,12 @@
+package com.keyin.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+    @Test
+    void login() {
+    }it
+}
